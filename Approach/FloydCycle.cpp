0198560cef5fbc7f49542832b60b992c6cc5716b
Mobile-first <aafreenparveen2575@gmail.com>
2026-01-10 20:09:01 +0530

    added folder

--- Approach/FloydCycle.cpp
+++ Approach/FloydCycle.cpp
@@ -1,6 +1,12 @@
 #include <vector>           
 using namespace std;
 
+//floydCycle detection algorithm detects cycle in a linked list and also finds the starting point of the cycle.
+//Here we are using this algorithm to find the duplicate number in an array where numbers are in the range 1 to n and array size is n+1.
+//The duplicate number will create a cycle in the linked list representation of the array.
+//Time complexity: O(n)
+//Space complexity: O(1)
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
@@ -16,6 +22,8 @@ public:
         
 
         // Phase 2: Find entrance to cycle (duplicate number)
+        //fast is still at intersection point
+        // we are only moving slow to the start of the array
         slow = nums[0];
         while (slow != fast) {
             slow = nums[slow];
